ae939f80f035a2467ed149474c84b223100cf914
saif <saif.ali@anywhere.co>
2023-08-16 19:58:08 +0530

    updated the code for drools

--- src/main/java/com/example/test/drool/config/DroolsConfig.java
+++ src/main/java/com/example/test/drool/config/DroolsConfig.java
@@ -1,22 +1,20 @@
 package com.example.test.drool.config;
 
 import org.drools.compiler.builder.conf.DecisionTableConfigurationImpl;
+import org.drools.decisiontable.SpreadsheetCompiler;
 import org.drools.drl.extensions.DecisionTableFactory;
 import org.drools.io.FileSystemResource;
 import org.kie.api.KieServices;
-import org.kie.api.builder.KieBuilder;
-import org.kie.api.builder.KieFileSystem;
-import org.kie.api.builder.KieModule;
-import org.kie.api.io.Resource;
 import org.kie.api.runtime.KieContainer;
 import org.kie.internal.builder.DecisionTableConfiguration;
-import org.kie.internal.io.ResourceFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
 import java.net.URL;
+import java.util.List;
 import java.util.Objects;
+
 
 @Configuration
 public class DroolsConfig {
@@ -26,4 +24,34 @@ public class DroolsConfig {
     public KieContainer kieContainer() {
         return kieServices.getKieClasspathContainer();
     }
+
+
+    public static void main(String[] args) {
+        todrl();
+    }
+
+    public static void todrl(){
+        DecisionTableConfiguration decisionTableConfiguration = new DecisionTableConfigurationImpl();
+        FileSystemResource
+                spreadsheetFile =
+                new FileSystemResource(getFile("rules/test.drl.xlsx"));
+
+        String generatedDrl = DecisionTableFactory.loadFromResource(spreadsheetFile, decisionTableConfiguration);
+
+        System.out.println();
+        System.out.println(" ######################## RULES FROM SPREADSHEET ######################## ");
+
+
+        System.out.println();
+        System.out.println(generatedDrl);
+        System.out.println();
+        System.out.println(" ######################## END ######################## ");
+        System.out.println();
+    }
+    private static File getFile(String fileName) {
+        ClassLoader classLoader = DroolsConfig.class.getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+        return new File(Objects.requireNonNull(resource).getFile());
+    }
+
 }
--- src/main/java/com/example/test/drool/model/WorkerModel.java
+++ src/main/java/com/example/test/drool/model/WorkerModel.java
@@ -1,26 +1,31 @@
 package com.example.test.drool.model;
 
 public class WorkerModel {
-    private String prevId;
-    private String actionType;
+    private String action;
+    private String type;
+    private String message;
 
-    public WorkerModel() {
-        this.actionType = "EVENT_CREATE";
+    public String getAction() {
+        return action;
     }
 
-    public String getPrevId() {
-        return prevId;
+    public void setAction(String action) {
+        this.action = action;
     }
 
-    public void setPrevId(String prevId) {
-        this.prevId = prevId;
+    public String getType() {
+        return type;
     }
 
-    public String getActionType() {
-        return actionType;
+    public void setType(String type) {
+        this.type = type;
     }
 
-    public void setActionType(String actionType) {
-        this.actionType = actionType;
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
