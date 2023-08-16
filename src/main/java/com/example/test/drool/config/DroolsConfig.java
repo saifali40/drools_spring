70870636dc16381934ecc0b2d16033416f162cb5
saif <saif.ali@anywhere.co>
2023-08-16 16:47:22 +0530

    updated the code for drools

--- src/main/java/com/example/test/drool/config/DroolsConfig.java
+++ src/main/java/com/example/test/drool/config/DroolsConfig.java
@@ -1,13 +1,24 @@
 package com.example.test.drool.config;
 
+import org.drools.compiler.builder.conf.DecisionTableConfigurationImpl;
+import org.drools.drl.extensions.DecisionTableFactory;
+import org.drools.io.FileSystemResource;
 import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.KieModule;
+import org.kie.api.io.Resource;
 import org.kie.api.runtime.KieContainer;
+import org.kie.internal.builder.DecisionTableConfiguration;
+import org.kie.internal.io.ResourceFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Objects;
 
 @Configuration
-@Lazy
 public class DroolsConfig {
     KieServices kieServices = KieServices.Factory.get();
 
@@ -15,5 +26,4 @@ public class DroolsConfig {
     public KieContainer kieContainer() {
         return kieServices.getKieClasspathContainer();
     }
-
 }
