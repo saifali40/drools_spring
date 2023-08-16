1561e18304581244c58710137be5ce4cab86aa68
saif <saif.ali@anywhere.co>
2023-08-16 11:55:46 +0530

    updated the code for drools

--- src/main/java/com/example/test/drool/TestControl.java
+++ src/main/java/com/example/test/drool/TestControl.java
@@ -3,9 +3,6 @@ package com.example.test.drool;
 
 import com.example.test.drool.model.WorkerModel;
 import com.example.test.drool.services.TestService;
-import org.kie.api.KieServices;
-import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.*;
